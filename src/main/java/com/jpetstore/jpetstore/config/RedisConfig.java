c572bfecade417837e60f1ad1e8a280878732043
funnyjoy <funnyjoy@P00074430-002.CNS.LGCNS.COM>
2020-10-01 00:55:38 +0900

    jpetstore 서비스 dockerize

--- src/main/java/com/jpetstore/jpetstore/config/RedisConfig.java
+++ src/main/java/com/jpetstore/jpetstore/config/RedisConfig.java
@@ -1,6 +1,8 @@
 package com.jpetstore.jpetstore.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -8,6 +10,13 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 
 @EnableRedisHttpSession
 public class RedisConfig {
+
+	@Value("${spring.redis.host}")
+	private String redisHost;
+
+	@Value("${spring.redis.port}")
+	private int redisPort;
+
 	/**
 	 * springboot 2.0이상부터는 auto-configuration으로 redisConnectionFactory,
 	 * RedisTemplate, StringTemplate빈들이 자동으로 생성되기 때문에 굳이 Configuration을 만들지 않아도 즉시
@@ -15,7 +24,14 @@ public class RedisConfig {
 	 */
 	@Bean
 	public LettuceConnectionFactory connectionFactory() {
-		return new LettuceConnectionFactory();
+
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName(redisHost);
+		redisStandaloneConfiguration.setPort(redisPort);
+		// 패스워드 있으면 설정
+//        redisStandaloneConfiguration.setPassword(redisPwd);
+		LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+		return lettuceConnectionFactory;
 	}
 
 	@Bean
